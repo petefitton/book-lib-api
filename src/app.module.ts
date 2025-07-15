82e4ff919ff961efa8dec49fa0eb193fd7f15500
Pete Fitton <petefitton@gmail.com>
2025-07-15 12:55:42 -0600

    Install npm packages

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // autoSchemaFile: 'schema.gql', // Generates schema automatically in memory
+      autoSchemaFile: join(process.cwd(), 'src/schema.graphql'),
+      playground: true,
+    }),
+    AuthorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
